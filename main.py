bd71c74a090482ae8ee4fc261ceb6ef4c0714fd9
AlexelAl <fordoor20@gmail.com>
2021-06-01 16:15:26 +0300

    die animation, start screen, more sound effects

--- main.py
+++ main.py
@@ -26,12 +26,28 @@ clock = pg.time.Clock()
 
 font_name = pg.font.match_font('arial')
 
-def draw_text(surf, text, size, x, y):
+def draw_text(surf, text, size, x, y, color):
     font = pg.font.Font(font_name, size)
-    text_surface = font.render(text, True, RED)
+    text_surface = font.render(text, True, color)
     text_rect = text_surface.get_rect()
     text_rect.midtop = (x, y)
     surf.blit(text_surface, text_rect)
+
+def show_go_screen():
+    screen.blit(background, background_rect)
+    draw_text(screen, "Flappy Bird!", 64, WIDTH / 2, HEIGHT / 4, BLUE)
+    draw_text(screen, "Tap to jump", 44,
+              WIDTH / 2, HEIGHT / 2.5, BLUE)
+    draw_text(screen, "Press a key to begin", 18, WIDTH / 2, HEIGHT * 3 / 4, BLUE)
+    pg.display.flip()
+    waiting = True
+    while waiting:
+        clock.tick(FPS)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+            if event.type == pg.KEYUP or event.type == pg.MOUSEBUTTONUP:
+                waiting = False
 
 class Bird(pg.sprite.Sprite):
     def __init__(self):
@@ -41,7 +57,13 @@ class Bird(pg.sprite.Sprite):
         self.image.set_colorkey(PURPLE)
         self.image =  pg.transform.scale(self.image, (50,40))
 
-        self.sprites = [pg.transform.rotate(self.image, 25),pg.transform.rotate(self.image, -25)]
+        self.dead_img = pg.image.load(path.join(img_dir, "Bird_dead.png")).convert()
+        self.dead_img.set_colorkey(PURPLE)
+        self.dead_img =  pg.transform.scale(self.dead_img, (50,40))
+
+        self.sprites = [pg.transform.rotate(self.image, 25),
+                        pg.transform.rotate(self.image, -25),
+                        pg.transform.rotate(self.dead_img, -75)]
 
         self.rect = self.image.get_rect()
         self.rect.x = 50
@@ -82,8 +104,12 @@ class Bird(pg.sprite.Sprite):
             self.image = self.sprites[1]
         self.rect.y = self.birdY
 
+        if self.dead:
+            self.image = self.sprites[2]
+
         if self.rect.y > 700 or self.rect.y < 0:
-            self.dead_act()
+            self.kill()
+
 
 class Column(pg.sprite.Sprite):
     def __init__(self, side):
@@ -95,6 +121,7 @@ class Column(pg.sprite.Sprite):
         self.gap = 135
         self.offset = random.randint(65,110)
         self.wallx_start = 400
+        self.speed = 2
 
         self.image = pg.image.load(path.join(img_dir, "Column1.png")).convert()
         self.image.set_colorkey(WHITE)
@@ -105,12 +132,13 @@ class Column(pg.sprite.Sprite):
         self.start()
 
     def update(self):
-        self.rect.x -= 2
+        self.rect.x -= self.speed
         if self.rect.x < -80:
             self.score += 1
             point_snd.play()
             self.start()
     def start(self):
+        self.speed = 2
         self.offset = random.randint(65,110)
         if self.side == "top":
             self.rect.y = 360 + self.gap - self.offset + 10
@@ -138,13 +166,29 @@ all_sprites.add(bird)
 hit_snd = pg.mixer.Sound(path.join(snd_dir, 'hit.mp3'))
 point_snd = pg.mixer.Sound(path.join(snd_dir, 'point.mp3'))
 wing_snd = pg.mixer.Sound(path.join(snd_dir, 'wing.mp3'))
+die_snd = pg.mixer.Sound(path.join(snd_dir, 'die.mp3'))
 
 pg.mixer.music.load(path.join(snd_dir, 'music.ogg'))
 pg.mixer.music.set_volume(0.4)
-
 pg.mixer.music.play(loops=-1)
+
+game_over = True
 running = True
 while running:
+    if game_over:
+        show_go_screen()
+        game_over = False
+        all_sprites = pg.sprite.Group()
+        columns = pg.sprite.Group()
+
+        for i in ["top","bot"]:
+            column = Column(i)
+            all_sprites.add(column)
+            columns.add(column)
+
+        bird = Bird()
+        all_sprites.add(bird)
+
     # Держим цикл на правильной скорости
     clock.tick(FPS)
     # Ввод процесса (события)
@@ -161,15 +205,21 @@ while running:
     hits = pg.sprite.spritecollide(bird , columns, False)
     if hits:
         bird.dead = True
+        for i in columns:
+            i.speed = 0.2
         if not bird.snd_indx:
             hit_snd.play()
             bird.snd_indx = True
+
+    if not bird.alive():
+        game_over = True
+        die_snd.play()
 
     # Рендеринг
     screen.fill(BLACK)
     screen.blit(background, background_rect)
     all_sprites.draw(screen)
-    draw_text(screen, str(column.score),  45, WIDTH / 2, 10)
+    draw_text(screen, str(column.score),  45, WIDTH / 2, 10, RED)
     # После отрисовки всего, переворачиваем экран
     pg.display.flip()
 
